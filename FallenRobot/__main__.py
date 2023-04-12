793e0fa3b6bed8082e838a64bc043b7f1da425d8
Hyper X Rachit <122621466+Hyper-x-Rachit@users.noreply.github.com>
2023-04-12 23:07:26 +0530

    Update __main__.py

--- FallenRobot/__main__.py
+++ FallenRobot/__main__.py
@@ -723,13 +723,13 @@ def main():
                 chat_id=f"@{SUPPORT_CHAT}",
                 photo=START_IMG,
                 caption=f"""
-ㅤ🥀 {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
+ㅤ⚡ {BOT_NAME} ɪs ᴀʟɪᴠᴇ ʙᴀʙʏ...
 
 ┏•❅────✧❅✦❅✧────❅•┓
-  ⚝ **ᴘʏᴛʜᴏɴ :** `{y()}`
-  ⚝ **ʟɪʙʀᴀʀʏ :** `{telever}`
-  ⚝ **ᴛᴇʟᴇᴛʜᴏɴ :** `{tlhver}`
-  ⚝ **ᴩʏʀᴏɢʀᴀᴍ :** `{pyrover}`
+ ㅤ⚝ **ᴘʏᴛʜᴏɴ :** `{y()}`
+ ㅤ⚝ **ʟɪʙʀᴀʀʏ :** `{telever}`
+ ㅤ⚝ **ᴛᴇʟᴇᴛʜᴏɴ :** `{tlhver}`
+ ㅤ⚝ **ᴩʏʀᴏɢʀᴀᴍ :** `{pyrover}`
 ┗•❅────✧❅✦❅✧────❅•┛""",
                 parse_mode=ParseMode.MARKDOWN,
             )
